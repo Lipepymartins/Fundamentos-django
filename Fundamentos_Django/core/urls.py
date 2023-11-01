b95120477af6fe5f1215782df8f969263906f8da
Lipepymartin <lipefenix2@gmail.com>
2023-11-01 14:29:50 -0300

    adicionando_formulario

--- Fundamentos_Django/core/urls.py
+++ Fundamentos_Django/core/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import hora_sistema
+from .views import hora_sistema, exibir_template
 
 urlpatterns = [
-    path('hora_sistema', hora_sistema)
+    path('hora_sistema', hora_sistema),
+    path('exibir_template', exibir_template),
 ]
--- Fundamentos_Django/core/views.py
+++ Fundamentos_Django/core/views.py
@@ -1,6 +1,8 @@
 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 import datetime 
+from .forms import ClienteForm
 
 # Create your views here.
 
@@ -8,3 +10,11 @@ import datetime
 def hora_sistema(request):
     hora_sistema = datetime.datetime.now()
     return render(request,'lista_horario.html', context={'hora_sistema':hora_sistema})
+
+def exibir_template(request):
+    if request.method == "POST":
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('hora_sistema')
+    form = ClienteForm()
+    return render(request, 'form_cliente.html', context={'form': form})
