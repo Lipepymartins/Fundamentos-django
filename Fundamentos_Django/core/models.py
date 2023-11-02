d4bd6f5573cb989c7e56e5e10d5aa58263d6142e
Lipepymartin <lipefenix2@gmail.com>
2023-11-02 11:19:41 -0300

    criando_models

--- Fundamentos_Django/core/models.py
+++ Fundamentos_Django/core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)#nome do clinete em charfild(para string com tamanho maximo de 100, nao pode ser nulo e não pode ser em branco)
+    idade = models.IntegerField(null=False, blank=True)
+    data_nascimento = models.DateField(null=False, blank=False)
+    is_ativo = models.BooleanField(null=False, default=True)#default(todo usuario que cadastrar estará ativo)
